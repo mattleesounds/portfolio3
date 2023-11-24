51ae5e2bb7a2cdcba0d0cbfd88971c110b8f2d9e
Matt Lee <mattleesounds@gmail.com>
2023-11-24 11:05:52 -0800

    Added 'All I Can Say' links

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -5,34 +5,35 @@ const page = () => {
   return (
     <div className="p-0 text-center h-screen flex flex-col ">
       <h1 className="text-3xl mb-2 mt-3">Links Du Matt</h1>
-      <h2 className="text-xl">{`Listen to "Too Loose"`}</h2>
+      <h2 className="text-xl">{`Listen to "All I Can Say"`}</h2>
       <div className="flex flex-col m-2 mb-0">
         <div className="mt-0 mb-8">
           <LinkButton
-            href="https://open.spotify.com/album/7JeBNnFbiFkSrxSAf4BKeV"
+            href="https://open.spotify.com/album/63cBEWxr6zJGRtlfA265nV?si=A2iwGJk7TZaZcDK1KE8qOQ"
             label="Spotify"
           />
         </div>
         <div className="mt-0 mb-8">
           <LinkButton
-            href="https://music.apple.com/us/album/too-loose-single/1706991531"
+            href="https://music.apple.com/us/album/all-i-can-say-single/1717985624"
             label="Apple Music"
           />
         </div>
-        <div className="mb-6">
+        {/*  <div className="mb-6">
           <LinkButton href="https://youtu.be/DbckiA089bE" label="Youtube" />
-        </div>
+        </div> */}
       </div>
-      <h2 className=" mb-0 p-2 text-xl">My blog</h2>
+      <h2 className="mb-0 text-xl">Website</h2>
+      <div className="mb-4 mt-0 p-2">
+        <LinkButton href="https://www.mattlee.world/" label="mattlee.world" />
+      </div>
+
+      <h2 className=" mb-0 p-2 text-xl">Blog</h2>
       <div className="mt-0 mb-6">
         <LinkButton
           href="https://paragraph.xyz/@matt"
           label="Le Blog Du Matt"
         />
-      </div>
-      <h2 className="mb-0 text-xl">My personal website</h2>
-      <div className="mb-4 mt-0 p-2">
-        <LinkButton href="https://www.mattlee.world/" label="mattlee.world" />
       </div>
     </div>
   );
